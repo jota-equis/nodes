47c624d33e59f1c8e945f8076e33f8fb6e54dee5
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-08-28 03:36:50 +0200

    Create base-config.sh

---
+++ node/cloud-init/base-config.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+# · ---
+export DEBIAN_FRONTEND=noninteractive
+# · ---
+SYS_LANG="${1:-es_ES}"
+SSH_PORT=22
+MASTER=
+DOMAIN=
+ROLE=
+TOKEN=
+EXTRAPORTS=
+REPO="https://raw.githubusercontent.com/jota-equis/nodes/main";
+# · ---
+echo -e "| CLOUD-FINISH ... :: start :: ..."
+# · ---
+usermod -L root;
+echo "root:$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)" | chpasswd;
+usermod -U root;
+
+mkdir -pm0751 /srv/{backup,data,local} /var/lib/{docker,longhorn,rancher} /mnt/tmp;
+mkdir -pm0751 /srv/local/{bin,etc/.env}; chmod 0710 /srv/local/etc/.env; 
+
+curl -o /etc/apt/apt.conf.d/999-local ${REPO}/node/config/etc/apt/apt.conf.d/999-local;
+curl -o /etc/fail2ban/jail.d/sshd.conf ${REPO}/node/config/etc/fail2ban/jail.d/sshd.conf;
+curl -o /etc/fail2ban/jail.d/portscan.conf ${REPO}/node/config/etc/fail2ban/jail.d/portscan.conf;
+curl -o /etc/ssh/sshd_config ${REPO}/node/config/etc/ssh/sshd_config && chmod 0600 /etc/ssh/sshd_config;
+curl -o /etc/sysctl.d/999-local.conf ${REPO}/node/config/etc/sysctl.d/999-local.conf;
+curl -o /etc/systemd/timesyncd.conf ${REPO}/node/config/etc/systemd/timesyncd.conf;
+
+for I in EXTRAPORTS DOMAIN MASTER REPO ROLE SSH_PORT SYS_LANG TOKEN; do [[ -z "${!I}" ]] && touch "/srv/local/etc/.env/${I}" || echo "${!I}" > "/srv/local/etc/.env/${I}"; done
+chmod 0600 /srv/local/etc/.env/*;
+
+if [[ "x${SSH_PORT}" != "x22" ]]; then
+    sed -i "/^Port 22/a Port ${SSH_PORT}" /etc/ssh/sshd_config;
+    sed -i "s/^port = 22$/&,${SSH_PORT}/" /etc/fail2ban/jail.d/sshd.conf;
+fi
+
+[[ ! -z "${DOMAIN}" ]] && sed -i "s/^#kernel.domainname/kernel.domainname           = ${DOMAIN}/g" /etc/sysctl.d/999-local.conf;
+
+sed -i 's/^#force_color_prompt/force_color_prompt/g' /etc/skel/.bashrc;
+sed 's/^Options=/Options=noexec,/g' /usr/share/systemd/tmp.mount > /etc/systemd/system/tmp.mount;
+
+localectl set-locale LANG=${SYS_LANG}.UTF-8 LANGUAGE=${SYS_LANG} LC_MESSAGES=POSIX LC_COLLATE=C;
+
+rm -Rf /tmp/* /tmp/.*;
+
+systemctl enable tmp.mount && systemctl start tmp.mount;
+systemctl restart systemd-timesyncd.service;
+systemctl enable fail2ban;
+# · ---
+DEBIAN_FRONTEND=noninteractive apt -y full-upgrade && apt -y autoclean && apt -y autoremove && sync;
+# · ---
+echo -e "| CLOUD-FINISH ... :: end :: ..."
+# · ---
+exit 0
