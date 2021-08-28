9bad53a2c927a2a65a263dd72722b5aa0e477859
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-08-28 13:12:24 +0200

    Update base-config.sh

--- node/cloud-init/base-config.sh
+++ node/cloud-init/base-config.sh
@@ -39,8 +39,8 @@ chmod 0750 /srv/local/bin/*;
 /srv/local/bin/local-ifaces.sh;
 
 if [[ "x${SSH_PORT}" != "x22" ]]; then
-    sed -i "/^Port 22/a Port ${SSH_PORT}" /etc/ssh/sshd_config;
-    sed -i "/Port 22/Port ${SSH_PORT}" /etc/ssh/ssh_config;
+    sed -i "s/^Port 22/a Port ${SSH_PORT}/" /etc/ssh/sshd_config;
+    sed -i "s/Port 22/Port ${SSH_PORT}/" /etc/ssh/ssh_config;
     sed -i "s/^port = 22$/&,${SSH_PORT}/" /etc/fail2ban/jail.d/sshd.conf;
 fi
 
