4ca5a1dac45d687f5e914b133cd96e8b80b9f712
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-08-30 01:14:04 +0200

    Update local-ifaces.sh

--- node/config/bin/local-ifaces.sh
+++ node/config/bin/local-ifaces.sh
@@ -8,6 +8,7 @@ for i in $(find /sys/class/net -type l -not -name eth0 -not -lname '*virtual*' -
   echo "${i}" >> ${CONF}/IFACE_LOCAL;
   echo "DEV=${i}" > ${CONF}/${i}.iface;
   ip -4 -f inet a show ${i} | awk '/inet/{ print "ADDR="$2 }' >> ${CONF}/${i}.iface;
+  ip -4 -f inet r | grep enp7s0 | grep via | awk '{ print $1 }' | awk -F  "/" '{ print "NETMASK="$2 }' >> ${CONF}/${i}.iface;
 done
 
 exit 0;
