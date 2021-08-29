5dc56329a494542df0e788d27e54cce75b3d8537
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-08-30 01:33:55 +0200

    Update local-netplan.sh

--- node/config/bin/local-netplan.sh
+++ node/config/bin/local-netplan.sh
@@ -6,14 +6,21 @@ for i in $(cat /srv/local/etc/.env/IFACE_LOCAL); do
   . /srv/local/etc/.env/${i}.iface;
 
   cp /srv/local/etc/netplan-local.yaml /etc/netplan/99-local-${DEV}.yaml;
-  
-  PREFIX=$(echo $ADDR |  awk -F  "." '{ print $1"."$2".0." }');
 
-  sed -i "s|ens00:|${DEV}:|g" /etc/netplan/99-local-${DEV}.yaml;
-  sed -i "s|192.168.0.|${PREFIX}|g" /etc/netplan/99-local-${DEV}.yaml;
-  sed -i "s|addresses:.*|addresses: [${ADDR}]|g" /etc/netplan/99-local-${DEV}.yaml;
+  if [[ "${NETMASK}" = "8" ]]; then
+    PREFIX=$(echo $ADDR |  awk -F  "." '{ print $1".0.0." }');
+  elif [[ "${NETMASK}" = "16" ]]; then
+    PREFIX=$(echo $ADDR |  awk -F  "." '{ print $1"."$2".0." }');
+  else
+    PREFIX=$(echo $ADDR |  awk -F  "." '{ print $1"."$2"."$3"." }');
+  fi
 
-  unset DEV ADDR PREFIX;
+  sed -i "s|IFNAME|${DEV}|g" /etc/netplan/99-local-${DEV}.yaml;
+  sed -i "s|IFROUTE|${PREFIX}1|g" /etc/netplan/99-local-${DEV}.yaml;
+  sed -i "s|IFNETWORK|${PREFIX}0/${NETMASK}|g" /etc/netplan/99-local-${DEV}.yaml;
+  sed -i "s|IFADDR|${ADDR}|g" /etc/netplan/99-local-${DEV}.yaml;
+
+  unset DEV ADDR PREFIX NETMASK;
 done
 
 netplan generate;
@@ -25,5 +32,7 @@ for n in $(cat /srv/local/etc/.env/IFACE_LOCAL); do
   echo -e "# [Route]\n# GatewayOnlink=true\n" >> /etc/systemd/network/${n}.d/override.conf;
   chmod 0644 /etc/systemd/network/${n}.d/override.conf;
 done
+
+netplan apply;
 
 exit 0;
