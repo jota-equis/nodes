8a18c0b09871c65c0f09ca31556e0cf0585b503a
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-09-25 08:18:06 +0200

    Update local-ifaces.sh

--- node/config/bin/local-ifaces.sh
+++ node/config/bin/local-ifaces.sh
@@ -3,12 +3,19 @@
 CONF=/srv/local/etc/.env
 
 [[ -f ${CONF}/IFACE_LOCAL ]] && rm ${CONF}/IFACE_LOCAL;
+[[ -f ${CONF}/LOCAL_CIDR ]] && LOCAL_CIDR=$(cat ${CONF}/LOCAL_CIDR);
 
 for i in $(find /sys/class/net -type l -not -name eth0 -not -lname '*virtual*' -printf '%f ' | tr " " "\n" | sort ); do
   echo "${i}" >> ${CONF}/IFACE_LOCAL;
   echo "DEV=${i}" > ${CONF}/${i}.iface;
   ip -4 -f inet a show ${i} | awk '/inet/{ print "ADDR="$2 }' >> ${CONF}/${i}.iface;
-  ip -4 -f inet r | grep enp7s0 | grep via | awk '{ print $1 }' | awk -F  "/" '{ print "NETMASK="$2 }' >> ${CONF}/${i}.iface;
+
+  if [[ ! -z $LOCAL_CIDR ]]; then
+    echo $LOCAL_CIDR | awk -F  "/" '{ print "NETWORK="$1"\nNETMASK="$2 }' >> ${CONF}/${i}.iface;
+  else
+    ip -4 -f inet r | grep ${i} | grep link | awk '{ print "NETWORK="$1 }' >> ${CONF}/${i}.iface;
+    ip -4 -f inet r | grep ${i} | grep via | awk '{ print $1 }' | awk -F  "/" '{ print "NETMASK="$2 }' >> ${CONF}/${i}.iface;
+  fi
 done
 
 exit 0;
